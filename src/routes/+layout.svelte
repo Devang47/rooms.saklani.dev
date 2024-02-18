aee61b475aa6c07df8ea0f0da6056110cda66d36
devang47 <72780418+Devang47@users.noreply.github.com>
2024-02-18 12:38:16 +0530

    partytown removed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,20 +3,13 @@
   import LoadingScreen from "$lib/sections/LoadingScreen.svelte";
   import { loading, user } from "$stores/app";
   import SvelteSeo from "svelte-seo";
-  import { partytownSnippet } from "@builder.io/partytown/integration";
   import { onMount } from "svelte";
   import { signInAnonymously } from "firebase/auth";
   import "$styles/global.scss";
   import { auth } from "$utils/config";
   import { addNotification } from "$utils/notifications";
 
-  // Add the Partytown script to the DOM head
-  let scriptEl: any;
   onMount(() => {
-    if (scriptEl) {
-      scriptEl.textContent = partytownSnippet();
-    }
-
     signInAnonymously(auth)
       .then((authUser) => {
         user.set(authUser.user);
@@ -27,20 +20,6 @@
       });
   });
 </script>
-
-<svelte:head>
-  <script>
-    partytown = {
-      forward: ["dataLayer.push"],
-      resolveUrl: (url) => {
-        const siteUrl = "https://rooms.saklani.dev/proxytown";
-        return url;
-      },
-    };
-  </script>
-
-  <script bind:this={scriptEl}></script>
-</svelte:head>
 
 {#if $loading}
   <LoadingScreen />
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { join } from "path";
-import { partytownVite } from "@builder.io/partytown/utils";
 import type { UserConfig } from "vite";
 import path from "path";
 
 const config: UserConfig = {
-  plugins: [
-    sveltekit(),
-    partytownVite({
-      // `dest` specifies where files are copied to in production
-      dest: join(process.cwd(), ".vercel/output/static", "~partytown"),
-    }),
-  ],
+  plugins: [sveltekit()],
   resolve: {
     alias: {
       $stores: path.resolve("./src/stores"),
