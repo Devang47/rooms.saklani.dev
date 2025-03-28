4458fb9f29fc80db461f55dad1866a7ed8dd10fe
devang47 <devangsaklani@gmail.com>
2025-03-28 14:30:24 +0530

    feat: updates added

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,11 +1,15 @@
 import { notifications } from "$stores";
 import { get } from "svelte/store";
 
-export let addNotification = (msg: string, error: boolean = false) => {
+export let addNotification = (
+  msg: string,
+  error: boolean = false,
+  timeout?: number,
+) => {
   notifications.set([...get(notifications), { data: msg, error }]);
 
   setTimeout(() => {
     get(notifications).shift();
     notifications.set(get(notifications));
-  }, 2000);
+  }, timeout ?? 2000);
 };
